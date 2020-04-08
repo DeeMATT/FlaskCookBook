0d97a31460d5bef04ac62d9794b91577b419519f
DeeMATT <mattode@outlook.com>
2020-04-08 02:24:22 +0100

    blueprint for modular

--- src/my_app/__init__.py
+++ src/my_app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)1
+app = Flask(__name__)
 
-import my_app.hello.views
+import my_app.hello.views import hello
+
+app = Flask(__name__)
+app.register_blueprint(hello)
